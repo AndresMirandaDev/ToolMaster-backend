66de234bb3c88a40dc041992671bb6b6fa73208f
Andres Miranda <andres.miranda.dev@gmail.com>
2023-02-16 12:18:22 +0900

    first commit with the winston logger set up

---
+++ startup/logging.js
@@ -0,0 +1,40 @@
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+
+module.exports = function () {
+  //uncaught exceptions logger
+  winston.exceptions.handle(
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+  );
+
+  process.on('unhandledRejection', (ex) => {
+    throw ex;
+  });
+  //error logger
+  winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+
+  //mongoDB logger
+  // winston.add(
+  //     new winston.transports.MongoDB({
+  //         db: connection string,
+  //         level:'error'
+
+  //     })
+  // )
+
+  //console logger
+  winston.add(
+    new winston.transports.Console({
+      handleExceptions: true,
+      format: winston.format.combine(
+        winston.format.printf((log) => {
+          log.message;
+        }),
+        winston.format.colorize(),
+        winston.format.prettyPrint(),
+        winston.format.simple()
+      ),
+    })
+  );
+};
